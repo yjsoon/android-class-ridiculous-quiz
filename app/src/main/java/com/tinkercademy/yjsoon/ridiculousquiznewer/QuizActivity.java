83a696e18ba7fc2ffac1c31c972f2906314032d0
YJ Soon <yjsoon@tinkertanker.com>
2015-02-24 11:23:22 +0800

    Renamed activity_main to activity_quiz as well

--- app/src/main/java/com/tinkercademy/yjsoon/ridiculousquiznewer/QuizActivity.java
+++ app/src/main/java/com/tinkercademy/yjsoon/ridiculousquiznewer/QuizActivity.java
@@ -38,7 +38,7 @@ public class QuizActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_quiz);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
